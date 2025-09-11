d32dccf8e51d4b311550c8e5377d3343ef8d9221
TEP3A <terzicnikola1989@gmail.com>
2025-09-11 21:32:46 +0200

    First test

--- WWT-Automation/AdminTool/SignIn.cs
+++ WWT-Automation/AdminTool/SignIn.cs
@@ -1,5 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Diagnostics;
 using WebDriverManager.DriverConfigs.Impl;
 
 namespace WWT_Automation.AdminTool
@@ -7,19 +10,26 @@ namespace WWT_Automation.AdminTool
     public class SignIn : IDisposable
     {
         IWebDriver driver;
+        WebDriverWait wait;
 
         [SetUp]
         public void StartBrowser()
         {
             new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
             driver = new ChromeDriver();
-            
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
         }
 
         [Test]
         public void SuccessfulSignIn()
         {
             driver.Url = "https://apadmintool.zero21.eu/";
+            driver.FindElement(By.Id("UserName")).SendKeys("SuperAdmin");
+            driver.FindElement(By.Id("Password")).SendKeys("T21kyytt$LVP#");
+
+            driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Name("topnavForm")));
         }
 
         public void Dispose()
